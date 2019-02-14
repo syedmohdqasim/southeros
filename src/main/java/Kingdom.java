b74199b58a3dfeaa58b78d473ed3649db054ee81
Syed <syedmq@thoughtworks.com>
2019-02-14 19:43:21 +0530

    add Kingdom

---
+++ src/main/java/Kingdom.java
@@ -0,0 +1,18 @@
+//Represents a main.java.Kingdom in universe of Southeros
+public class Kingdom {
+    private String name;
+    private String animal;
+
+    public Kingdom(String name, String animal) {
+        this.name = name;
+        this.animal = animal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+}
---
+++ src/test/java/KingdomTest.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KingdomTest {
+
+    private Kingdom kingdomOne;
+    private String kingdomeOne = "kingdomeOne";
+    private String animalOne = "animalOne";
+
+    @Before
+    public void setUp() throws Exception {
+        kingdomOne = new Kingdom(kingdomeOne, animalOne);
+    }
+
+    @Test
+    public void shouldReturnCorrectNameOfKingdom() {
+        assertEquals(kingdomOne.getName(), kingdomeOne);
+    }
+
+    @Test
+    public void shouldReturnCorrectNameOfAnimal() {
+        assertEquals(kingdomOne.getAnimal(), animalOne);
+    }
+}
